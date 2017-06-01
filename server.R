0671c89ec1356103d61fefa2815000040cf02787
Nikhila Iyer <Nikhila@D-173-250-162-17.dhcp4.washington.edu>
2017-06-01 14:39:01 -0700

    libraries and scripts added

---
+++ server.R
@@ -0,0 +1,20 @@
+#server.R
+
+#libraries
+library(dplyr)
+library(rgeos)
+library(shiny)
+library(plotly)
+library(httr)
+library(stringr)
+library(rjson)
+library(jsonlite)
+library(anytime)
+
+#scripts
+source('scripts/setup.R')
+
+#shinyserver
+shinyServer(function(input, output) {
+  
+})
