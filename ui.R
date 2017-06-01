53aa6777765d57415cf3b4b22b7ba5376ea37c15
Mara Childs <Mara@D-173-250-235-138.dhcp4.washington.edu>
2017-06-01 14:48:27 -0700

    ui changes

--- ui.R
+++ ui.R
@@ -1,12 +1,13 @@
 # Read in libraries
 library(shiny)
 library(plotly)
+library(shinythemes)
 
 # Read in source scripts
 source('scripts/setup.R')
 
 # Create Shiny UI
-shinyUI(fluidPage(
+shinyUI(fluidPage(theme = shinytheme("superhero"),
   
   # Application title
   titlePanel("Twitter and Weather"),
@@ -20,15 +21,7 @@ shinyUI(fluidPage(
       selectInput("city", "Select City", choices = cities),
       
       # Returns YYYY-MM-DD
-      dateInput("start.date", "Select Date", max = Sys.Date()),
-      
-      # Returns YYYY-MM-DD
-      dateInput("end.date", "Select End Date", min = min.end, max = max.end, value = max.end),
-      
-      # Returns 1 (Tweets) or 2 (Weather) or 3 (Both)
-      radioButtons("radio", label = "Charts to Display",
-                   choices = list("Tweets" = 1, "Weather" = 2, "Both" = 3), 
-                   selected = 2)),
+      dateInput("date", "Select Date", max = Sys.Date()),
     
     # Plot it!
     mainPanel(
@@ -44,7 +37,7 @@ shinyUI(fluidPage(
         # About panel
         tabPanel("About", textOutput('about'))
       )
-      
+      )
     )
   )
 )
