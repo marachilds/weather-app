5df1effa42bafaae2417b0c8188f013cad7c012e
Mara Childs <Mara@Maras-MacBook-Pro-Retina.local>
2017-06-01 21:08:33 -0700

    put build in server

--- server.R
+++ server.R
@@ -15,7 +15,7 @@ library(shinythemes)
 # Scripts
 source('scripts/setup.R')
 #source('scripts/analysis.R')
-source('scripts/build.R')
+#source('scripts/build.R')
 
 # shinyServer
 shinyServer(function(input, output) {
@@ -23,7 +23,28 @@ shinyServer(function(input, output) {
   #Plot
   output$mainPlot <- renderPlotly({
     
-    plotly_build("thePlot")
+    my.data <- 
+    
+    x <- list(
+      title = "Hour"
+    )
+    
+    thePlot <- plot_ly(my.data, 
+                       x = ~time,
+                       y = ~temperature, 
+                       colors = "PuRd",
+                       type = 'scatter', 
+                       mode = 'lines+markers',
+                       hoverinfo = 'text',
+                       text = ~paste0('Location: ', ~get(input$city),
+                                      '</br>', "Time: ", my.data$time.only,
+                                      '</br>', "Temperature ", my.data$temperature)) %>%
+      layout(title = "Title Here", 
+             xaxis = x) %>% 
+      add_trace(y = ~windSpeed, name = 'Wind Speed', mode = 'lines+markers') %>% 
+      add_trace(y = ~cloudCover, name = 'Cloud Coverage', mode = 'lines+markers')
+    
+    plotly_build(thePlot)
     
   })
 
